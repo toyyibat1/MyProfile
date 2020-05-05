986d1f90c1228bf251f59fee69803c402582b4b4
unknown <toyyibat.abdulkareem@gmail.com>
2020-05-05 23:01:57 +0100

    user can login and email is passed to profile activity

---
+++ app/src/main/java/com/toyyibat/myprofile/LoginActivity.kt
@@ -0,0 +1,125 @@
+package com.toyyibat.myprofile
+
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.app.LoaderManager.LoaderCallbacks
+import android.content.ContentResolver
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build.VERSION
+import android.os.Build
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.text.TextUtils
+import android.view.KeyEvent
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import java.util.ArrayList
+import android.Manifest.permission.READ_CONTACTS
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity() {
+    // UI references.
+    private var mEmailView: EditText? = null
+    private var mPasswordView: EditText? = null
+    private var checkBoxRememberMe: CheckBox? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Set up the login form.
+        mEmailView = findViewById<View>(R.id.email) as EditText
+        mPasswordView = findViewById<View>(R.id.password) as EditText
+        mPasswordView!!.setOnEditorActionListener(TextView.OnEditorActionListener { textView, id, keyEvent ->
+            if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                attemptLogin()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+        val mEmailSignInButton =
+            findViewById<View>(R.id.email_sign_in_button) as Button
+        mEmailSignInButton.setOnClickListener { attemptLogin() }
+        checkBoxRememberMe = findViewById<View>(R.id.checkBoxRememberMe) as CheckBox
+        //Here we will validate saved preferences
+        if (!PrefManager(this).isUserLogedOut()) { //user's email and password both are saved in preferences
+            startMainActivity()
+        }
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() { // Reset errors.
+        mEmailView!!.error = null
+        mPasswordView!!.error = null
+        // Store values at the time of the login attempt.
+        val email = mEmailView!!.text.toString()
+        val password = mPasswordView!!.text.toString()
+        var cancel = false
+        var focusView: View? = null
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView!!.error = getString(R.string.error_invalid_password)
+            focusView = mPasswordView
+            cancel = true
+        }
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView!!.error = getString(R.string.error_field_required)
+            focusView = mEmailView
+            cancel = true
+        } else if (!isEmailValid(email)) {
+            mEmailView!!.error = getString(R.string.error_invalid_email)
+            focusView = mEmailView
+            cancel = true
+        }
+        if (cancel) { // There was an error; don't attempt login and focus the first
+// form field with an error.
+            focusView!!.requestFocus()
+        } else { // save data in local shared preferences
+            if (checkBoxRememberMe!!.isChecked) saveLoginDetails(email, password)
+            startMainActivity()
+        }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun saveLoginDetails(email: String, password: String) {
+        PrefManager(this).saveLoginDetails(email, password)
+    }
+
+    private fun isEmailValid(email: String): Boolean { //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean { //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+}
---
+++ app/src/main/java/com/toyyibat/myprofile/SignUpActivity.kt
@@ -0,0 +1,4 @@
+package com.toyyibat.myprofile
+
+class SignUpActivity {
+}
